e02397255fd2d1740d50f289e1176888ea4a77d4
Sudhir Rathod <125038605+sudhirrathod03@users.noreply.github.com>
2026-04-06 10:47:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 function shortcut(s1, s2) {
   // your code here
+	if(s1.length === 0  || s2.length=== 0){
+		return '';
+	}
+
+		let ans = s1[0] + s2[0];
+	   return ans;
+	
 }
 
 // Do not change the code below.
 const s1 = prompt("Enter s1:");
+
 const s2 = prompt("Enter s2:");
 alert(shortcut(s1, s2));
